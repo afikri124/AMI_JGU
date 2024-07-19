8fd4dba1ee8162ae8ff8068f74b142d68dccd1c1
rrandul <rofiqabdul983@gmail.com>
2024-07-19 11:10:59 +0700

    update profile dikit

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -21,7 +21,7 @@
                         </ul>
                         <div class="tab-content" id="icon-tabContent">
                               <div class="tab-pane fade active show text-center pt-0" id="icon-home" role="tabpanel" aria-labelledby="icon-home-tab">
-                                    <a href="{{asset('assets/img/AMI_JGU Diagram .png')}}" target="_blank">
+                                    <a href="{{asset('assets/img/AMI_JGU Diagram.png')}}" target="_blank">
                                     <img class="img-fluid" style="max-height: 450px;" src="{{asset('assets/img/AMI_JGU Diagram.png')}}">
                                     </a>
                               </div>
